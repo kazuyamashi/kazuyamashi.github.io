1574f83655bfbb967cb2e34ad1443d7ad6583853
kazuyamashi <kazuyamashi@gmail.com>
2017-04-02 22:18:10 +0900

    update

--- make.py
+++ make.py
@@ -9,7 +9,7 @@ def generate_index_html_japanese():
 	index = open("index.html","w")
 
 	profile = unicode(open(TEMPLATE_ja+"profile.html","r").read(),encoding='utf-8')
-	works = unicode(open(TEMPLATE_ja+"works.html","r").read(),encoding='utf-8')
+	works 	= unicode(open(TEMPLATE_ja+"works.html","r").read(),encoding='utf-8')
 	studies = unicode(open(TEMPLATE_ja+"studies.html","r").read(),encoding='utf-8')
 	contact = unicode(open(TEMPLATE_ja+"contact.html","r").read(),encoding='utf-8')
 
@@ -20,6 +20,7 @@ def generate_index_html_japanese():
 							'works': works,
 							'studies': studies,
 							'contact': contact }).encode('utf-8')
+	
 	index.write(html_string)
 	print "Generated index.html"
 
@@ -28,7 +29,7 @@ def generate_index_html_english():
 	index = open("index_en.html","w")
 
 	profile = unicode(open(TEMPLATE_en+"profile.html","r").read(),encoding='utf-8')
-	works = unicode(open(TEMPLATE_en+"works.html","r").read(),encoding='utf-8')
+	works 	= unicode(open(TEMPLATE_en+"works.html","r").read(),encoding='utf-8')
 	studies = unicode(open(TEMPLATE_en+"studies.html","r").read(),encoding='utf-8')
 	contact = unicode(open(TEMPLATE_en+"contact.html","r").read(),encoding='utf-8')
 
@@ -39,6 +40,7 @@ def generate_index_html_english():
 							'works': works,
 							'studies': studies,
 							'contact': contact }).encode('utf-8')
+
 	index.write(html_string)
 	print "Generated index_en.html"
 
